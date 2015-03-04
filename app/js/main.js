0fc7129fd551c80f4b394855e6c79e74afc327bd
Sergey <sramator@gmail.com>
2015-03-04 09:53:51 +0200

    add body

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,42 @@
 $(document).ready(function(){
-  $('.bxslider').bxSlider({
-  	'pager' : false,
-  	'controls' : true,
-  	'auto' : true,
-  	'pause' : 6500
-  });
+
+
+
+	var app = {
+
+			initialize : function () {
+				this.setUpListeners();
+				this.slider;
+			},
+			slider: $('#bxslider'),
+			addCart: $('#header__price'),
+			headerMunu: $('#header__menu').find('li'),
+
+			// setUpListeners
+			setUpListeners: function () {
+				this.slider.bxSlider({
+				  	'pager' : false,
+				  	'controls' : true,
+				  	'auto' : true,
+				  	'pause' : 6500
+				  });
+				this.addCart.on('click', this.buy); // appear blco with buy
+				this.headerMunu.on('click', this.menuActive);
+
+			},
+
+			/* functions */
+			buy: function(){
+				var showBlock = $('#header__buy');
+				if (showBlock.hasClass('hide')) {
+					showBlock.removeClass('hide');
+				} else
+				showBlock.addClass('hide');
+			},
+			menuActive: function() {
+				var $this = $(this)
+				$this.addClass('header__menu-item_active').siblings().removeClass('header__menu-item_active');
+			},
+	}
+	app.initialize();
 });
---
+++ app/js/placeholder.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    /* Placeholder for IE */
+
+    $("form").find("input[type='text']").each(function() {
+        var tp = $(this).attr("placeholder");
+        $(this).attr('value',tp).css('color','#ccc');
+    }).focusin(function() {
+        var val = $(this).attr('placeholder');
+        if($(this).val() == val) {
+            $(this).attr('value','').css('color','#303030');
+        }
+    }).focusout(function() {
+        var val = $(this).attr('placeholder');
+        if($(this).val() == "") {
+            $(this).attr('value', val).css('color','#ccc');
+        }
+    });
+
+});
